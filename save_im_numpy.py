3afe910087c722693fa8105b5d65166fb1841df4
Venkat Korapaty <venkat.korapaty@gmail.com>
2018-03-20 15:47:36 -0400

    save_im_numpy now crops to the smaller dimension of an images l/w, then applies blur/NN interp.

--- save_im_numpy.py
+++ save_im_numpy.py
@@ -7,18 +7,18 @@ from PIL import Image
 from PIL import ImageFilter
 import scipy.misc
 
-
 def main(unused_arg):
-    gaussian_blur = 2
+    gaussian_blur = 1
     path = "./datasets/VOC_2007/traindata/VOC2007/JPEGImages/"
     path_to_image_names = "./datasets/VOC_2007/traindata/VOC2007/ImageSets/Main/aeroplane_train.txt"
     # image_names = os.listdir(path)
     image_names = open(path_to_image_names).readlines()
 
     amount_of_train_images = len(image_names)
+    nn_size = 227
 
-    padded_images = np.zeros(shape=(amount_of_train_images, 500, 500, 3))
-    blurred_padded_images = np.zeros(shape=(amount_of_train_images, 500, 500, 3))
+    cropped_nn_images = np.zeros(shape=(amount_of_train_images, nn_size, nn_size, 3))
+    #nn_images = np.zeros(shape=(amount_of_train_images, nn_size, nn_size, 3))
     sess = tf.InteractiveSession()
     # for i in range(len(image_names)):
     print("Starting padding/blurring images..")
@@ -30,62 +30,90 @@ def main(unused_arg):
         # image = mpimg.imread(image_loc)
         image = Image.open(image_loc)
 
+        image_size = image.size
+        resize_num = 500
+        if (image_size[0] > image_size[1]):
+            resize_num = image_size[1]
+        else:
+            resize_num = image_size[0]
+
+
         # Pad image
-        t = tf.image.resize_image_with_crop_or_pad(image, 500, 500)
-        padded = t.eval()
-
-        # Convert from numpy to PIL image
-        pil_padded_image = Image.fromarray(padded)
-
-        # Blur image
-        blurred = pil_padded_image.filter(ImageFilter.GaussianBlur(gaussian_blur))
-
-        # Store in respective arrays to be saved
-        padded_images[i] = padded
-        blurred_padded_images[i] = blurred
-
-    print("Saving padded/blurred..")
-    np.save('./VOC_data/voc07_train_only_padded.npy', padded_images)
-    np.save('./VOC_data/voc07_train_only_blurred_padded.npy', blurred_padded_images)
-
-    padded_images = None
-    blurred_padded_images = None
-
-    cropped_images = np.zeros(shape=(amount_of_train_images, 227, 227, 3))
-    blurred_cropped_images = np.zeros(shape=(amount_of_train_images, 227, 227, 3))
-
-    image_names = open(path_to_image_names).readlines()
-
-    sess = tf.InteractiveSession()
-    # for i in range(len(image_names)):
-    print("Starting cropping/blurring images..")
-    for i in range(amount_of_train_images):
-        print(i)
-        # Get image
-        filename = image_names[i].split(' ')[0] + '.jpg'
-        image_loc = path + "/" + filename
-        # image = mpimg.imread(image_loc)
-        image = Image.open(image_loc)
-
-        # crop image
-        t = tf.image.resize_image_with_crop_or_pad(image, 227, 227)
+        t = tf.image.resize_image_with_crop_or_pad(image, resize_num, resize_num)
         cropped = t.eval()
 
         # Convert from numpy to PIL image
-        pil_cropped_image = Image.fromarray(padded)
+        pil_cropped_image = Image.fromarray(cropped)
 
         # Blur image
         blurred = pil_cropped_image.filter(ImageFilter.GaussianBlur(gaussian_blur))
 
+        # Apply nearest neighbour interpolation
+        p = tf.convert_to_tensor(blurred)
+        nn_im = tf.image.resize_nearest_neighbor([p],
+            (nn_size, nn_size))
+
         # Store in respective arrays to be saved
-        cropped_images[i] = cropped
-        blurred_cropped_images[i] = blurred
+        cropped_nn_images[i] = nn_im[0].eval()
 
-    print("Saving cropped/blurred..")
-    np.save('./VOC_data/voc07_train_only_cropped.npy', cropped_images)
-    np.save('./VOC_data/voc07_train_only_blurred_cropped.npy', blurred_cropped_images)
+        # blur image, not using padding
+        #blurred = image.filter(ImageFilter.GaussianBlur(gaussian_blur))
+        # Convert back to tensor and do nearest_neighbor interpolation
+        #p = tf.convert_to_tensor(blurred)
+        #nn_im = tf.image.resize_nearest_neighbor([p],
+         #   (nn_size, nn_size))
+        # nn_images[i] = nn_im[0].eval()
 
+    print("Saving padded/blurred..")
     sess.close()
+    np.save('./VOC_data/voc07_train_only_blurred_nn_cropped.npy', cropped_nn_images)
+    print("Done!")
+    # np.save('./VOC_data/voc07_train_only_blurred_nn.npy', nn_images)
+
+    #padded_images = None
+    # blurred_padded_images = None
+
+    # path = "./datasets/VOC_2007/testdata/VOC2007/JPEGImages"
+    # path_to_image_names = "./datasets/VOC_2007/testdata/VOC2007/ImageSets/Main/aeroplane_test.txt"
+    # image_names = open(path_to_image_names).readlines()
+    # amount_of_train_images = len(image_names)
+
+    # ## NOTE: images aren't actually cropped, they are padded, but called cropped
+    # ## as the intention was to use this code to crop images to 227*227 and then use gaussian blur
+    # cropped_images = np.zeros(shape=(amount_of_train_images, 500, 500, 3))
+    # blurred_cropped_images = np.zeros(shape=(amount_of_train_images, 500, 500, 3))
+
+    # sess = tf.InteractiveSession()
+    # # for i in range(len(image_names)):
+    # print("Starting cropping/blurring images..")
+    # for i in range(amount_of_train_images):
+    #     print(i)
+    #     # Get image
+    #     filename = image_names[i].split(' ')[0] + '.jpg'
+    #     image_loc = path + "/" + filename
+    #     # image = mpimg.imread(image_loc)
+    #     image = Image.open(image_loc)
+
+    #     # crop image
+    #     t = tf.image.resize_image_with_crop_or_pad(image, 500, 500)
+    #     cropped = t.eval()
+
+    #     # Convert from numpy to PIL image
+    #     pil_cropped_image = Image.fromarray(cropped)
+
+    #     # Blur image
+    #     blurred = pil_cropped_image.filter(ImageFilter.GaussianBlur(gaussian_blur))
+
+    #     # Store in respective arrays to be saved
+    #     cropped_images[i] = cropped
+    #     blurred_cropped_images[i] = blurred
+
+    # print("Saving cropped/blurred..")
+    # sess.close()
+    # np.save('./VOC_data/voc07_test_only_padded.npy', cropped_images)
+    # np.save('./VOC_data/voc07_test_only_blurred_cropped.npy', blurred_cropped_images)
+
+    #sess.close()
 
 
     # traindata = np.load("./VOC_data/voc07_train1.npy")
