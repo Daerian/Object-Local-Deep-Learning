3e3c064e0a33f9dd4387c6f6cd2d5c4419efbd0b
Venkat Korapaty <venkat.korapaty@gmail.com>
2018-03-31 20:03:21 -0400

    did greedy algorithm for localization

--- test_net.py
+++ test_net.py
@@ -8,6 +8,7 @@ import os
 from PIL import Image
 from PIL import ImageFilter
 import scipy.misc as sm
+from queue import *
 
 # Convolutional Layer 1
 filter_size1 = 5
@@ -270,32 +271,157 @@ def run_net(y_labs, y_true, restore):
         # sm.imsave("./pre_proc_im.jpg", pre_proc_im[0])
 
         # cv_im = 101
-        local_cand = 0
-        for cv_im in range(2510):
-            pre_proc_im = cv[cv_im,:]
-            label_true = cv_labels[cv_im,:]
-            sm.imsave("./pre_proc_im.jpg", pre_proc_im)
+        # local_cand = 0
+        # for cv_im in range(2510):
+        #     print(cv_im)
+        #     pre_proc_im = cv[cv_im,:]
+        #     label_true = cv_labels[cv_im,:]
+        #     sm.imsave("./pre_proc_im.jpg", pre_proc_im)
             
-            computed_logits = session.run(logits, feed_dict={x: [pre_proc_im]})
-            print(computed_logits)
-            # y_class = tf.nn.sigmoid(computed_logits)
-            y_class = session.run(tf.nn.softmax(computed_logits))
-            max_cls = np.argmax(y_class)
-            print(max_cls)
-            y_class = np.zeros(shape=(1, 20))
-            y_class[0,max_cls] = 1
-            # y_pred = session.run(tf.cast(tf.round(y_class), tf.int64))
-            # y_pred_probs = session.run(y_class)
-            print("True labels:")
-            print(label_true)
-            print("Prediction:")
-            print(y_class)
-            # print(y_pred_probs)
-            # print()
-            if np.sum((y_class == 1) == (label_true == 1)) == 20:
-                print(cv_im)
-                local_cand = cv_im
-                break
+        #     computed_logits = session.run(logits, feed_dict={x: [pre_proc_im]})
+        #     # print(computed_logits)
+        #     # y_class = tf.nn.sigmoid(computed_logits)
+        #     y_class = session.run(tf.nn.softmax(computed_logits))
+        #     print(y_class)
+        #     max_cls = np.argmax(y_class)
+        #     print(max_cls)
+        #     y_class = np.zeros(shape=(1, 20))
+        #     y_class[0,max_cls] = 1
+        #     # y_pred = session.run(tf.cast(tf.round(y_class), tf.int64))
+        #     # y_pred_probs = session.run(y_class)
+        #     print("True labels:")
+        #     print(label_true)
+        #     print("Prediction:")
+        #     print(y_class)
+        #     # print(y_pred_probs)
+        #     # print()
+        #     if np.sum((y_class == 1) == (label_true == 1)) == 20:
+        #         print(cv_im)
+        #         local_cand = cv_im
+        #         break
+
+        
+        # LOCALIZATION
+
+        #split into 4
+        pre_proc_im = cv[23,:]
+
+        max_loc_itters = 40
+
+
+        comp_logits = session.run(logits, feed_dict={x: [pre_proc_im]})
+        y_class = session.run(tf.nn.softmax(comp_logits))
+        fmax = y_class[0, 0]
+
+        cands = Queue()
+        cands.put(pre_proc_im)
+        i = 0
+        CLASS = 14
+        while cands.empty() == False and i < max_loc_itters:
+            print(i)
+            i += 1
+            
+            candidate = np.asarray(cands.get())
+            print(candidate.shape)
+
+            
+            candidatep = session.run(tf.image.resize_image_with_crop_or_pad(candidate,227,227))
+            comp_logits = session.run(logits, feed_dict={x: [candidatep]})
+            y_class = session.run(tf.nn.softmax(comp_logits))
+            prob_y_class = y_class[0, CLASS]
+            
+            sh = candidate.shape
+            rows = sh[0]
+            cols = sh[1]
+
+            c1 = np.delete(candidate, 0, axis=0)
+            c1p = session.run(tf.image.resize_image_with_crop_or_pad(c1,227,227))
+            c2 = np.delete(candidate, (rows-1), axis=0)
+            c2p = session.run(tf.image.resize_image_with_crop_or_pad(c2,227,227))
+            c3 = np.delete(candidate, 0, axis=1)
+            c3p = session.run(tf.image.resize_image_with_crop_or_pad(c3,227,227))
+            c4 = np.delete(candidate, (cols-1), axis=1)
+            c4p = session.run(tf.image.resize_image_with_crop_or_pad(c4,227,227))
+            
+
+
+
+
+            top = np.zeros(shape = [1,4])
+            # top[0,0] = prob_y_class
+            
+            cl = session.run(logits, feed_dict={x: [c1p]})
+            score = session.run(tf.nn.softmax(cl))
+            score = score[0, CLASS]
+            top[0,0] =  score
+
+            cl = session.run(logits, feed_dict={x: [c2p]})
+            score = session.run(tf.nn.softmax(cl))
+            score = score[0, CLASS]
+            top[0,1] =  score
+
+            cl = session.run(logits, feed_dict={x: [c3p]})
+            score = session.run(tf.nn.softmax(cl))
+            score = score[0, CLASS]
+            top[0,2] =  score
+
+            cl = session.run(logits, feed_dict={x: [c4p]})
+            score = session.run(tf.nn.softmax(cl))
+            score = score[0, CLASS]
+            top[0,3] =  score
+
+            print (top)
+
+            mx = np.max(top)
+            print ("max prob: " + str(mx) + ", top[0,0]: " + str(top[0,0]))
+            l = mx > top[0,0]
+            print(l)
+
+            # if mx > top[0,0]:
+            which = np.argmax(top)
+
+            if which == 0:
+                print("c1 pushed")
+                cut = c1
+                cands.put(c1)
+            
+            if which == 1:
+                print("c2 pushed")
+                cut = c2
+                cands.put(c2)
+            
+            if which == 2:
+                print("c3 pushed")
+                cut = c3
+                cands.put(c3)
+            
+            if which == 3:
+                print("c4 pushed")
+                cut = c4
+                cands.put(c4)
+
+            if i == max_loc_itters:
+                print("Saving..")
+                sm.imsave("./localized_pic.jpg", cut)
+
+            # else:
+            #     print("BREAKING")
+            #     sm.imsave("./localized_pic_break.jpg", candidate)
+            #     break
+
+
+
+
+             
+
+
+
+
+
+
+
+
+
         
     else:
         init = tf.global_variables_initializer()
